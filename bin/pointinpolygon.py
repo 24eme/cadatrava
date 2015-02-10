e1dbcd158b725cd354096b3a3f91a67fe540b6d1
Tangui Morlier <tangui@tangui.eu.org>
2015-02-10 19:38:07 +0100

    Find a point in a geojson polygon

---
+++ bin/pointinpolygon.py
@@ -0,0 +1,34 @@
+import json
+import sys
+import numpy as np
+from shapely.geometry import shape, Point
+
+f = open(sys.argv[1], 'r')
+js = json.load(f)
+f.close()
+
+lineid = 0;
+for feature in js['features']:
+    feature['geometry']['type'] = 'Polygon'
+    feature['geometry']['coordinates'] = [feature['geometry']['coordinates']]
+    polygon = shape(feature['geometry'])
+    selectedpoint1 = None
+    selectedpoint2 = None
+    for x in np.arange(polygon.bounds[0], polygon.bounds[2], (polygon.bounds[2] - polygon.bounds[0]) / 10):
+        for y in np.arange(polygon.bounds[1], polygon.bounds[3], (polygon.bounds[3] - polygon.bounds[1]) / 10):
+            point = Point(x, y)
+            if polygon.contains(point):
+                if selectedpoint1:
+                    selectedpoint2 = point
+                else:
+                    selectedpoint1 = point
+                break
+        if selectedpoint2:
+            break
+    if selectedpoint2:
+        print "%s : %d : %s : %f,%f %f,%f" % (sys.argv[1], lineid, feature['id'], selectedpoint1.x, selectedpoint1.y, selectedpoint2.x, selectedpoint2.y)
+    elif selectedpoint1:
+        print "%s : %d : %s : %f,%f" % (sys.argv[1], lineid, feature['id'], selectedpoint1.x, selectedpoint1.y)        
+    else:
+        print >> sys.stderr, "%s : %d : %s : no point found :-(" % (sys.argv[1], lineid, feature['id'])
+    lineid = lineid + 1
