e7c7355bb93e20a3bbaf7559fae279288ce4efbd
Tangui Morlier <tangui@tangui.eu.org>
2015-02-16 12:43:37 +0100

    Ajout de cache + suppression de l'export des images

--- bin/cadastre_solver.sh
+++ bin/cadastre_solver.sh
@@ -2,14 +2,31 @@
 
 COOKIEFILE=/tmp/.cookies.$$
 
+cd $(dirname $0)"/.."
+
 if ! test "$1"  || ! test "$2" || ! test "$3" || ! test "$4" ; then
 	echo "USAGE: $0 <commune> <section de la parcelle> <numero de la parcelle> <code postal de la commune>" >&2
 	exit 1
 fi
 
-curl -s -k -c $COOKIEFILE http://www.cadastre.gouv.fr/scpc/accueil.do > /dev/null
-
 DEP=$(echo $4 | sed 's/\(..\).*/\1/');
+
+CACHE="cache/"$DEP"_"$1"_"$2"_"$3".txt"
+
+if test -s $CACHE; then
+	if grep ERROR $CACHE > /dev/null; then
+	        echo "Parcelle ERROR ($1 $2 $3 $4)";
+		exit 1;
+	fi
+	echo -n "Parcelle OK ($1 $2 $3 $4) : ";
+        PARCELLE_REF=$(grep REFERENCE $CACHE | sed 's/.*: //')
+	PARCELLE_SUPERFICIE=$(grep SUPERFICIE $CACHE | sed 's/.*: //')
+	PARCELLE_ID=$(grep _ID $CACHE | sed 's/.*: //')
+	echo $PARCELLE_REF $PARCELLE_SUPERFICIE $PARCELLE_ID
+	exit 0;
+fi
+
+curl -s -k -c $COOKIEFILE http://www.cadastre.gouv.fr/scpc/accueil.do > /dev/null
 
 #Soumission du formulaire de recherche de parcelle
 echo -n "<?php echo urlencode(iconv('UTF-8', 'ASCII//TRANSLIT','" > /tmp/.ville.$$.php ; echo -n $1 >> /tmp/.ville.$$.php ; echo "'));" >> /tmp/.ville.$$.php
@@ -19,6 +36,7 @@ rm /tmp/.ville.$$.php
 #Vérifie que la parcelle existes
 if ! grep afficherCarteParcelle.do /tmp/parcellaire.$$.html > /dev/null ; then
 	echo "Parcelle ERROR ($1 $2 $3 $4)";
+	echo ERROR > $CACHE
 	exit 1;
 fi
 echo -n "Parcelle OK ($1 $2 $3 $4) : ";
@@ -26,6 +44,14 @@ echo -n "Parcelle OK ($1 $2 $3 $4) : ";
 #Depuis le résultats de recherche de la parcelle, ouvre la popup de carto
 URL=$(cat /tmp/parcellaire.$$.html | grep afficherCarteParcelle.do | sed 's/.*afficherCarteParcelle.do/afficherCarteParcelle.do/'| sed "s/','.*//" | sed 's/amp;//');
 PARCELLE_ID=$(echo $URL | sed 's/.*p=//' | sed 's/&f=.*//')
+if test -s $PARCELLE_ID".txt" ; then
+        PARCELLE_REF=$(grep REFERENCE $PARCELLE_ID".txt"  | sed 's/.*: //')
+        PARCELLE_SUPERFICIE=$(grep SUPERFICIE $PARCELLE_ID".txt" | sed 's/.*: //')
+        PARCELLE_ID=$(grep _ID $PARCELLE_ID".txt" | sed 's/.*: //')
+	echo $PARCELLE_REF $PARCELLE_SUPERFICIE $PARCELLE_ID
+        cp $PARCELLE_ID".txt" $CACHE
+	exit;
+fi
 curl -s -k -b $COOKIEFILE -c $COOKIEFILE".2" -H 'Referer: http://www.cadastre.gouv.fr/scpc/rechercherParReferenceCadastrale.do' "http://www.cadastre.gouv.fr/scpc/"$URL > /tmp/parcellaire.$$.html
 #Récupère les coordonnées Lamber de la parcelle (et ajoute un peu de marge)
 PARCELLE_BBOX=$(echo -n $(grep -A 4 GeoBox /tmp/parcellaire.$$.html  | head -n 10 | tail -n 4 | sed 's/)//') | sed 's/ //g' | sed 's/,$//'| perl -e '@bbox = split ",", <STDIN>; print $bbox[0]-1; print ",";print $bbox[1]-1; print ","; print $bbox[2]+1; print "," ; print $bbox[3]+1;')
@@ -38,12 +64,15 @@ echo $PARCELLE_REF $PARCELLE_SUPERFICIE	$PARCELLE_ID
 PARCELLE_LATLONG_X=$(echo $PARCELLE_BBOX | sed 's/,/ /' | sed 's/,.*//' | invproj +proj=lcc +lat_1=47.25 +lat_2=48.75 +lat_0=48 +lon_0=3 +x_0=1700000 +y_0=7200000 +units=m +to +proj=latlong +units=m -f '%.20f' | sed 's/ *//' | sed 's/\t/,/')
 PARCELLE_LATLONG_Y=$(echo $PARCELLE_BBOX | sed 's/,/ /' |sed 's/[^,]*,//' | sed 's/,/ /' | invproj +proj=lcc +lat_1=47.25 +lat_2=48.75 +lat_0=48 +lon_0=3 +x_0=1700000 +y_0=7200000 +units=m +to +proj=latlong +units=m -f '%.12f' | sed 's/ *//' | sed 's/\t/,/')
 #Conserve les méta données dans un fichier texte
-echo "COMMUNE: $1" > $PARCELLE_ID.txt
-echo "PARCELLE_REFERENCE: $PARCELLE_REF" >> $PARCELLE_ID.txt
-echo "PARCELLE_SUPERFICIE: $PARCELLE_SUPERFICIE" >> $PARCELLE_ID.txt
-echo "PARCELLE_ID: $PARCELLE_ID" >> $PARCELLE_ID.txt
-echo "PARCELLE_BBOX_IGN: $PARCELLE_BBOX" >> $PARCELLE_ID".txt"
-echo "PARCELLE_BBOX_LATLONG: "$PARCELLE_LATLONG_X","$PARCELLE_LATLONG_Y >>  $PARCELLE_ID".txt"
+echo "COMMUNE: $1" > $PARCELLE_ID.txt.tmp
+echo "PARCELLE_REFERENCE: $PARCELLE_REF" >> $PARCELLE_ID.txt.tmp
+echo "PARCELLE_SUPERFICIE: $PARCELLE_SUPERFICIE" >> $PARCELLE_ID.txt.tmp
+echo "PARCELLE_ID: $PARCELLE_ID" >> $PARCELLE_ID.txt.tmp
+echo "PARCELLE_BBOX_IGN: $PARCELLE_BBOX" >> $PARCELLE_ID".txt.tmp"
+echo "PARCELLE_BBOX_LATLONG: "$PARCELLE_LATLONG_X","$PARCELLE_LATLONG_Y >>  $PARCELLE_ID".txt.tmp"
+mv $PARCELLE_ID".txt.tmp" $CACHE
+cp $CACHE $PARCELLE_ID".txt"
+exit
 #Demande l'image de la parcelle
 curl -s -k -b $COOKIEFILE -c $COOKIEFILE".2" -H 'Referer: http://www.cadastre.gouv.fr/scpc/'$URL -X GET "http://www.cadastre.gouv.fr/scpc/wms?version=1.1&request=GetMap&layers=CDIF:PARCELLE&format=image/png&bbox="$PARCELLE_BBOX"&width=1000&height=500&exception=application/vnd.ogc.se_inimage&styles=PARCELLE_90&selection=PARCELLE_90,null,PARCELLE_SELECTION_90,PARCELLE,"$PARCELLE_ID > /tmp/parcellaire.$$.png
 mv /tmp/parcellaire.$$.png $PARCELLE_ID.png
